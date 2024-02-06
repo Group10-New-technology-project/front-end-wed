888c722948eb40fe0346880340efc34fb443a9ea
BuiNgocTung <buingoctung1990@gmail.com>
2024-02-06 10:21:13 +0700

    fix

--- src/layouts/LoginLayout/LoginLayout.js
+++ src/layouts/LoginLayout/LoginLayout.js
@@ -1,5 +1,9 @@
 import Header from './Header';
-import './LoginLayout.scss';
+import classNames from 'classnames/bind';
+import styles from './LoginLayout.module.scss';
+import './LoginLayout.module.scss';
+
+const cx = classNames.bind(styles);
 function LoginLayout({ children }) {
     return (
         <div className={cx('wrapper')}>
