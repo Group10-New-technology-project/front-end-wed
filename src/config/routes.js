ce9a66b45d8e5fa0d48101c0aff4aa02429173d9
BuiNgocTung <buingoctung1990@gmail.com>
2024-02-06 19:34:52 +0700

    update config/routes.js,/routes/routes.js

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,6 +2,10 @@ const routes = {
     home: '/',
     profile: '/profile',
     login: '/login',
+    signIn: '/signin',
+    otpSignIn: '/otpSignIn',
+    forgotPassword: '/forgotPassword',
+    otpForgotPassword: '/otpForgotPassword',
 };
 
 export default routes;
